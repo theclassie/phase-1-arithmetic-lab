431625b1636b83dd75b7960e86c7601da275ebd9
Tristyn Broadhead <trisclassie@gmail.com>
2022-11-06 19:34:17 -0700

    commit tree ec2a2401a5dfc8dc65db85fc1490f27781e853bf

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 3;
+let num4 = 12;
+let mod = num4 / num3;
+
+let max = Math.max(5, 10, 15, 20);
